834cff0f0e023c4f6a6f2454bfe526dcadb27f7e
jackmacdougall <Apple?1997>
2020-02-05 14:12:53 -0500

    tests

--- src/main/java/addressbook/SmokeTest.java
+++ src/main/java/addressbook/SmokeTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class AccessingDataJpaApplicationTest {
+public class SmokeTest {
 
     @Autowired
     private AddressBookController controller;
